185fea25ddf89490ba0411b275dab823c4b47ccf
RedPandaSmash <redpandasmash@gmail.com>
2025-07-03 16:39:03 -0400

    first steps, changed type to modules from commonjs to enable import

---
+++ controllers/routes.js
@@ -0,0 +1,5 @@
+//get all comments from  blog.json file.
+//get one comment  by its post_id
+//post: create a new entry which will be appended to the .json file's outermost array.
+//put: update an existing entry once a match has been found. The search should be done via a query parameter, whereas update information should be enclosed within the body.
+// delete an entry from our blog.json file. This should be done thru the utilization of the parameter.
